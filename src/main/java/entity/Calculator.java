cd1a363d8f0ee4e74ed3b6016d43f042e06841f0
viktor <nikolaviktor747@gmail.com>
2018-07-30 16:04:53 +0300

    I will uploude that files for the Web Calculator

---
+++ src/main/java/entity/Calculator.java
@@ -0,0 +1,58 @@
+package entity;
+
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public Calculator(double leftOperand, double rightOperand, String operator) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+        this.operator = operator;
+    }
+    public double CalculateResult(){
+        double result = 0;
+
+        if (this.operator.equals("+")){
+           result = leftOperand + rightOperand;
+        }
+        if (this.operator.equals("-")){
+
+            result = leftOperand - rightOperand;
+        }
+        if (this.operator.equals("*")){
+            result = leftOperand * rightOperand;
+        }
+        if (this.operator.equals("/")){
+            result = leftOperand / rightOperand;
+        }
+        return  result;
+
+
+    }
+
+}
